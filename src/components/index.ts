a861eed0d8711fa9e791e7e777397771cfd3b689
Martin Janys <martinjanys@gmail.com>
2016-03-06 18:40:03 +0100

    update components module

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,23 +1,37 @@
 require('./animations.scss');
 
-import btn from './button';
 import badge from './badge';
+import btn from './button';
+import collapsible from './collapsible';
 import collection from './collection';
 import collectionItem from './collection-item';
 import dropdown from './dropdown';
 import dropdownItem from './dropdown-item';
+import dropdownList from './dropdown-list';
+import eventWrapper from './event-wrapper';
+import icon from './icon';
+import leanOverlay from './lean-overlay';
+import modal from './modal';
 import navbar from './navbar';
 import navbarItem from './navbar-item';
+import sidenavOverlay from './sidenav-overlay';
 
 const VueMaterializeComponents = {
-    btn,
     badge,
+    btn,
+    collapsible,
     collection,
     collectionItem,
     dropdown,
     dropdownItem,
+    dropdownList,
+    eventWrapper,
+    icon,
+    leanOverlay,
+    modal,
     navbar,
-    navbarItem
+    navbarItem,
+    sidenavOverlay
 };
 
 export default VueMaterializeComponents;

0b20d2775ae8bcd2e5300d0e860afae4812e71ce
Martin Janys <martinjanys@gmail.com>
2016-03-06 08:44:00 +0100

    doc index.html navbar

--- src/docs/doc-app.ts
+++ src/docs/doc-app.ts
@@ -4,8 +4,11 @@ import VueModule = require('vue')
 var Vue: any = VueModule;
 
 import pages = require('./pages');
+import components from '../components';
 
+// because vue router not work with TS @Component
 export = Vue.extend({
+    components: components,
     data: function () {
         return {
             navs: pages
